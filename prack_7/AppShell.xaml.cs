29de964314d990cded99f36fc2046c3e03aca41e
ILYA <ILYA@WIN-U756ECU82RH>
2023-11-15 12:22:59 +0300

    Добавьте файлы проекта.

---
+++ prack_7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace prack_7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ prack_7/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace prack_7
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPhone_Clicked(object sender, EventArgs e)
+        {
+            //добавляем телефоны в секцию телефоны
+            EntryCell p = new EntryCell();
+            p.Placeholder = "Введите телефон";
+            p.Keyboard = Keyboard.Numeric;
+            phone.Add(p);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateBirth_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            int ag = DateTime.Now.Year - dateBirth.Date.Year;
+            if (DateTime.Now.Month < dateBirth.Date.Month || DateTime.Now.Month == dateBirth.Date.Month && DateTime.Now.Day < dateBirth.Date.Day)
+                   
+            {
+                ag--;
+                age.Text =  "Возраст - " + ag.ToString ();
+            }
+        }
+    }
+}
